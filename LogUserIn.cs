0bc0c28b978c855df45f2474240c9678e78a86ee
Katie Jones <klhowlettjones@gmail.com>
2022-12-04 21:03:46 +0000

    Created LogUserIn class

---
+++ LogUserIn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Investment_ideas_platform
+{
+    class LogUserIn
+    {
+        //Check that the user's email is in the database. If they have an account, let them log in
+        public static bool userExists(string email)
+        {
+            if (DBConnection.getInstanceOfDBConnection().emailExists(Constants.CHECK_EXISTS_userEmail, email))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
