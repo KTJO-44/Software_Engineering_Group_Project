7822c5800f389563d3c038524b695396ea5caaf6
Katie Jones <klhowlettjones@gmail.com>
2022-12-04 17:36:46 +0000

    Added constants class

---
+++ Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Investment_ideas_platform
+{
+    class Constants
+    {
+        public static string FETCH_USER_FIRSTNAME = "SELECT firstName FROM staffAccounts WHERE email=@userEmail";
+    }
+}
