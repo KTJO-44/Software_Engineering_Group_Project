fca7a6e130ce294587e2b5cb52e5fad9802f3574
Nathan Hina <92038573+Debilitatum@users.noreply.github.com>
2022-12-05 13:58:48 +0000

    Started GUI for idea creator platform

---
+++ idea_creator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Investment_ideas_platform
+{
+    public partial class idea_creator : Form
+    {
+        public idea_creator()
+        {
+            InitializeComponent();
+        }
+    }
+}
