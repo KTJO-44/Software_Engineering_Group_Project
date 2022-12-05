3fa9188b85fbcfaa9c846c731c9644903faa5095
Nathan Hina <92038573+Debilitatum@users.noreply.github.com>
2022-12-05 15:01:21 +0000

    Created a log_out button Login created to idea creator account Updated comments based on changes

--- idea_creator.cs
+++ idea_creator.cs
@@ -6,18 +6,26 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+// Form created to make idea creator page
+// This will house idea creator capabilities
+// Partial class will enable public merge between forms allowing to switch from form to form in future
+
 namespace Investment_ideas_platform
 {
-    public partial class idea_creator : Form
+    public partial class idea_creator : Form // Form created
     {
-        public idea_creator()
+        public idea_creator() 
         {
             InitializeComponent();
         }
+        
+        // Adding a button to the form 
+        // Named as btn_logout with behaviour activated on click
+        // Used to log out of the application
 
-        private void btn_logout_Click(object sender, EventArgs e)
+        private void btn_logout_Click(object sender, EventArgs e) // button event based on user click
         {
-            Application.Exit();
+            Application.Exit(); // Exit application 
         }
     }
 }
