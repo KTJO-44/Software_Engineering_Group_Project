3880490f17c12032b442eb5ceae807a014b1b580
Katie <80467102+KTJO-44@users.noreply.github.com>
2022-12-10 17:37:21 +0000

    Added more comments

--- CreateResetNotification.cs
+++ CreateResetNotification.cs
@@ -6,22 +6,27 @@ namespace Investment_ideas_platform
 {
     class CreateResetNotification
     {
-        //Use DBConnection to find StaffID, we know notID, then CREATE notification, 2 spearate funcs?
+        //Use DBConnection to find StaffID, we know notificationID, then CREATE notification
         public static bool findStaffID(string email)
         {
+            //creating an int value then fetching the staffID into it, using the email of the account
             int staffID = DBConnection.getInstanceOfDBConnection().getSingleIntUsingJustEmail(Constants.FETCH_STAFFID, email);
+            //If a reset password notificaiton already exists in the database, return true so the program knows not to create another one
             if (DBConnection.getInstanceOfDBConnection().specificNotificationExists(Constants.CHECK_EXISTS_SPECIFIC_NOTIFICATION, staffID))
             {
                 return true;
             }
+            //otherwise return false, and the program can go ahead and create a new reset notification associated with that staff accounts
             return false;
         }
 
+        //Method to make the reset notificaiton
         public static void makeResetNotification(string email)
         {
-            //We have already clarified that the user account exists.
+            //We have already clarified that the user account exists
+            //Fetch the staffID using the staff account's email
             int staffID = DBConnection.getInstanceOfDBConnection().getSingleIntUsingJustEmail(Constants.FETCH_STAFFID, email);
-            //now we make the notification itself.
+            //Making the notification. We know the notificaitonID of a reset notification is 1, and we have just fetched the staffID
             DBConnection.getInstanceOfDBConnection().createNotification(Constants.CREATE_RESET_NOTIFICATION, 1, staffID);
         }
 
--- DeleteAccount.cs
+++ DeleteAccount.cs
@@ -6,9 +6,12 @@ namespace Investment_ideas_platform
 {
     class DeleteAccount
     {
+        //Method to delete a staff account
         public static bool deleteStaffAccount(string staffEmail)
         {
+            //Deletes a staff account using the staff's email
             DBConnection.getInstanceOfDBConnection().deleteAccountFromDB(Constants.DELETE_STAFF_ACCOUNT, staffEmail);
+            //return true so that the program knows it was successful
             return true;
         }
     }
